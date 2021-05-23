e3e88b9b7d99ed3fe33140e162022a36ddc0b8ec
Nicholas <nichz123@gmail.com>
2021-05-23 17:40:01 +0700

    Done adding the map interaction on the transaction detail page. Now create the edit transaction page

--- Trallet/Controllers/TransactionDetailController.swift
+++ Trallet/Controllers/TransactionDetailController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import MapKit
 
 class TransactionDetailController: UIViewController {
     @IBOutlet weak var transactionType: UILabel!
     @IBOutlet weak var transactionNotes: UILabel!
     @IBOutlet weak var transactionLocationImage: UIImageView!
     @IBOutlet weak var transactionLocation: UILabel!
+    @IBOutlet weak var transLocationMap: MKMapView!
+    @IBOutlet weak var mapHeight: NSLayoutConstraint!
     
     @IBOutlet weak var thumbPaymentMethod: UIImageView!
     @IBOutlet weak var txtPaymentMethod: UILabel!
@@ -35,14 +38,33 @@ class TransactionDetailController: UIViewController {
         numberFormat.maximumFractionDigits = 2
 //        numberFormat.currencyCode = currencyCode
         
+        // Set label for Category and Notes
         transactionType.text = cdTransaction.transCategory
         transactionNotes.text = cdTransaction.transNotes ?? ""
+        
+        // Transaction Location Text, image and Pinpoint
         transactionLocation.text = cdTransaction.transLocationKeyword ?? ""
         
-        if cdTransaction.transLocationKeyword == nil {
+        // Set the map height to 0 of no keyword
+        if cdTransaction.transLocationKeyword == nil && cdTransaction.transLocationItem == nil {
             transactionLocationImage.image = nil
+            mapHeight.constant = 0
+        } else {
+            // Set the pinpoint
+            if let pinpoint = cdTransaction.transLocationItem as? MKMapItem {
+                let pinpoint = cdTransaction.transLocationItem as! MKMapItem
+                let annotation = LocationAnnotation(object: pinpoint)
+                transLocationMap.addAnnotation(annotation)
+                
+                // Set the map zoom
+                let coordinate = pinpoint.placemark.coordinate
+                let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                let region = MKCoordinateRegion(center: coordinate, span: span)
+                self.transLocationMap.setRegion(region, animated: true)
+            }
         }
         
+        // Set payment method image and text
         txtPaymentMethod.text = cdTransaction.transPaymentMethod ?? ""
         switch cdTransaction.transPaymentMethod {
         case "Cash":
@@ -53,18 +75,21 @@ class TransactionDetailController: UIViewController {
             thumbPaymentMethod.image = nil
         }
         
+        // Set Payment Amount Text
         txtPaymentAmount.text = "\(currencyCode!) \(numberFormat.string(from: NSNumber(value: cdTransaction.transAmount))!)"
         thumbIncomeExpense.image = cdTransaction.transType == "Income" ? UIImage(systemName: "plus.circle.fill") : UIImage(systemName: "minus.circle.fill")
         thumbIncomeExpense.tintColor = cdTransaction.transType == "Income" ? UIColor.systemGreen : UIColor.systemRed
         
         arrImages = cdTransaction.transAttachments as? [UIImage] ?? [UIImage]()
         
+        // Collection View Flow Layout
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: attachmentCollectionView.frame.size.width / 3 - 2, height: attachmentCollectionView.frame.size.width / 3 - 2)
-        layout.scrollDirection = .vertical
+        layout.scrollDirection = .horizontal
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         layout.minimumInteritemSpacing = 1
         
+        // Set collection view layout
         attachmentCollectionView.collectionViewLayout = layout
         attachmentCollectionView.delegate = self
         attachmentCollectionView.dataSource = self
@@ -73,17 +98,15 @@ class TransactionDetailController: UIViewController {
     @IBAction func btnDonePressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
     }
-    */
-
 }
 
 extension TransactionDetailController: UICollectionViewDelegate, UICollectionViewDataSource, UIViewControllerPreviewingDelegate {
@@ -116,4 +139,8 @@ extension TransactionDetailController: UICollectionViewDelegate, UICollectionVie
     func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
         print("")
     }
+}
+
+extension TransactionDetailController: MKMapViewDelegate {
+    
 }
